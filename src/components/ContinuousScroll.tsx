d3153409ea4addff30b25c4ea9c74c3700871c47
Ajay6549 <ambekar4442@gmail.com>
2025-12-07 21:29:25 +0530

    update code

--- src/components/ContinuousScroll.tsx
+++ src/components/ContinuousScroll.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 import icon1 from "../../public/Images/icon1.png";
 import icon2 from "../../public/Images/icon2.png";
 import icon3 from "../../public/Images/icon3.png";
 import icon4 from "../../public/Images/icon4.png";
-
 
 const items = [
   { icon: icon1, text: "+ to Access to 700M+ Contacts" },
@@ -13,27 +12,32 @@ const items = [
   { icon: icon4, text: "Advanced Waterfall Enrichment" },
 ];
 
-
-
 const ContinuousScroll = () => {
-  return (
-      <div className=''>
-         <div className="overflow-hidden relative w-full">
-      <div className="flex animate-scroll whitespace-nowrap">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="flex items-center space-x-2 px-6 "
-          >
-            <img src={item.icon} alt={item.text} className="w-6 h-6" />
-            <span className="text-gray-800 font-medium">{item.text}</span>
-          </div>
-        ))}
-    
-      </div>
-    </div> 
-      </div> 
-  )
-}
+  const duplicatedItems = [...items, ...items]; // infinite loop effect
 
-export default ContinuousScroll
+  return (
+    <div className="w-full py-14 bg-white">
+      <div className="relative overflow-hidden">
+   
+        {/* Scroll Row */}
+        <div className="flex animate-scroll whitespace-nowrap">
+          {duplicatedItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center bg-white rounded-xl shadow-sm mx-4 px-6 "
+            >
+              <div className="w-10 h-10 flex items-center justify-center bg-blue-50 rounded-lg">
+                <img src={item.icon} alt={item.text} className="w-6 h-6 object-contain" />
+              </div>
+              <span className="ml-4 text-gray-800 font-semibold text-base">
+                {item.text}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContinuousScroll;
